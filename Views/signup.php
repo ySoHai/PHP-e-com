8b3cea355663281f861e06b08e9f81a20ae18ee5
ySoHai <edward.n25@gmail.com>
2022-11-08 21:06:19 -0800

    Created sign up page
    Created sign up page

---
+++ Views/signup.php
@@ -0,0 +1,35 @@
+<?php require_once('header.php'); ?>
+      <!--  contact -->
+      <div class="contact">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>Sign Up</h2>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                  <form id="request" class="main_form">
+                     <div class="row">
+                        <div class="col-md-12 ">
+                           <input class="contactus" placeholder="Enter Email" type="type" name="Email"> 
+                        </div>
+                        <div class="col-md-12">
+                           <input class="contactus" placeholder="Enter Password" type="password" name="password"> 
+                        </div>
+                        <div class="col-md-12">
+                           <input class="contactus" placeholder="Confirm Password" type="password" name="confirmpassword"> 
+                        </div>
+                        <div class="col-md-12">
+                           <button class="send_btn">Sign Up</button>
+                        </div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end contact -->
+<?php require_once('footer.php'); ?>
