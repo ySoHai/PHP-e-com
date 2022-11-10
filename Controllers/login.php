2e629c4c1d9e88de188233c062d12c798ac47542
thevossenberg <117322172+thevossenberg@users.noreply.github.com>
2022-11-09 17:59:42 -0800

    login error

--- Controllers/login.php
+++ Controllers/login.php
@@ -4,10 +4,10 @@ require_once('../Models/database.php');
 require_once('../Models/user_db.php');
 
 if (validateLogin($_POST['email'], $_POST['password'])) {
-	include_once("Location: ../Views/index.php");
+	include_once("../Views/index.php");
 }
 else {
 	$error_login = 'Invalid!';
-	include_once("Location: ../Views/login.php");
+	include_once("../Views/login.php");
 }
 ?>
--- Controllers/signup.php
+++ Controllers/signup.php
@@ -4,5 +4,5 @@ require_once('../Models/database.php');
 require_once('../Models/user_db.php');
 
 registerUser($_POST['email'], $_POST['phoneNumber'], $_POST['address'], $_POST['firstName'], $_POST['lastName'], $_POST['password']);
-include_once("Location: ../Views/index.php");
+include_once("../Views/index.php");
 ?>
