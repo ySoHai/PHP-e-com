10437c7ebb04045edec62c65f7edcb38ebe48b6c
ySoHai <edward.n25@gmail.com>
2022-11-12 19:22:59 -0800

    Update login.php

--- Controllers/login.php
+++ Controllers/login.php
@@ -4,13 +4,17 @@ require_once('../Models/database.php');
 require_once('../Models/user_db.php');
 
 if (validateLogin($_POST['email'], $_POST['password'])) {
-	include_once("../Views/index.php");
-	
+	$userId = getUserId($_POST['email']);
 	$lifetime = 60 * 60 * 24 * 30;    // 30 days in seconds
 	session_set_cookie_params($lifetime, '/');
 	session_start();
+	$_SESSION['userId'] = $userId['userID'];
+	header('Location: ../index.php');
+	
+
+	//session_start();
 	//$_SESSION['loggedin'] = true;
-	$_SESSION['userId'] = getUserId($_POST['email']);
+	//$_SESSION['userId'] = getUserId($_POST['email']);
 	
 }
 else {
