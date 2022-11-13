edbbde06b258adcc5169f290be49bfd6e40157cf
ySoHai <edward.n25@gmail.com>
2022-11-12 20:21:14 -0800

    fixed syntax

--- Views/account.php
+++ Views/account.php
@@ -5,7 +5,7 @@
             <div class="row">
                <div class="col-md-12">
                   <div class="titlepage">
-                     <h2>Contact</h2>
+                     <h2>My Account</h2>
                   </div>
                </div>
             </div>
