9c480f9e3eb8ac7c7087eb77aff7074778316c8a
ySoHai <edward.n25@gmail.com>
2022-11-12 20:39:20 -0800

    added delete cookies
    need to test

--- Controllers/logout.php
+++ Controllers/logout.php
@@ -1,11 +1,17 @@
 <?php
 session_start(); 
 session_destroy();
-/*
-if(isset($_COOKIE[session_name()])):
-    setcookie(session_name(), '', time()-7000000, '/');
-endif;
-*/
+
+if(isset($_COOKIE[session_name()])){
+    $name = session_name();  // get session cookie name 
+    $expire = new DateTime('-1 year'); // set expire date in the past
+    $params = session_get_cookie_params(); // get session paramas
+    $path = $params['/'];
+    setcookie($name, '', $expire, $path);
+}
+
+
+
 header('Location: ../Views/index.php');
 exit();
 ?>
