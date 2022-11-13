5f8a72bb3bc47f8c882cb6edaba2117c3f3978a5
ySoHai <edward.n25@gmail.com>
2022-11-12 20:33:12 -0800

    Update login.php

--- Controllers/login.php
+++ Controllers/login.php
@@ -1,8 +1,8 @@
 <?php
-
 require_once('../Models/database.php');
 require_once('../Models/user_db.php');
 
+// might change this to a class if we have time 
 if (validateLogin($_POST['email'], $_POST['password'])) {
 	if (session_status() === PHP_SESSION_NONE) {
 		$lifetime = 60 * 60 * 24 * 1;    // 1 days in seconds
