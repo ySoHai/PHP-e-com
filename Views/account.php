d4e2b19fc6b38dda5f5b101dc43269d205c6c7ab
ySoHai <39169678+ySoHai@users.noreply.github.com>
2022-11-16 18:17:56 -0800

    Update account.php

--- Views/account.php
+++ Views/account.php
@@ -1,5 +1,5 @@
 <?php require_once('header.php'); 
-include_once ('../Models/user_class.php');
+require_once ($model_path.'user_class.php');
 ?>
       <!--  account -->
       <div class="contact">
