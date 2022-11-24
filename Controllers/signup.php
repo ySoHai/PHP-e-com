8125681accf1ce5bd6f45dc04bd11522f8bc953b
Drclarke99 <111334576+Drclarke99@users.noreply.github.com>
2022-11-24 14:17:10 -0800

    updated sign up to function correctly

--- Controllers/signup.php
+++ Controllers/signup.php
@@ -3,9 +3,9 @@
 require_once('../Models/database.php');
 require_once('../Models/user_db.php');
 
-if (!empty($_POST['phoneNumber'])&&!empty($_POST['address'])&&!empty($_POST['firstName'])&&!empty($_POST['lastName'])&&!empty($_POST['password'])&&validateEmail($_POST['email'])) {
+if (!empty($_POST['phoneNumber'])&&!empty($_POST['address'])&&!empty($_POST['firstName'])&&!empty($_POST['lastName'])&&!empty($_POST['password'])) {
 	registerUser($_POST['email'], $_POST['phoneNumber'], $_POST['address'], $_POST['firstName'], $_POST['lastName'], $_POST['password']);
-	header('Location: ../Views/account.php');
+	header('Location: ../Views/index.php');
 }
 else {
 	$error_signup = true;
