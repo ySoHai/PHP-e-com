6c33abd1970ebe707cf24cc394c2cdd7a7224a53
thevossenberg <117322172+thevossenberg@users.noreply.github.com>
2022-11-25 10:06:21 -0800

    fixes

--- Controllers/list.php
+++ Controllers/list.php
@@ -1,9 +1,15 @@
 <?php
 session_start();
+
+if(!isset($_SESSION['userId'])){
+	header("Location: ../Views/login.php");
+	die('Something went very wrong :(');
+}
+
 require_once('../Models/database.php');
 require_once('../Models/product_db.php');
 
-if (!empty($_POST['prodName']) && !empty($_POST['prodDesc']) && !empty($_POST['price']) && !empty($_POST['quantity']) && !empty($_POST['shipDays'])) {
+if (!empty($_POST['prodName']) && !empty($_POST['prodDesc']) && !empty($_POST['price']) && !empty($_POST['quantity']) && !empty($_POST['quality']) &&!empty($_POST['shipDays']) && !empty($_POST['category'])) {
 	ProductDB::addProduct($_POST['prodName'], $_POST['prodDesc'], $_POST['price'], $_POST['quantity'], $_POST['quality'], $_POST['shipDays'], $_POST['category'], ((int)$_SESSION['userId']));
 	header('Location: ../Views/account.php');
 }
