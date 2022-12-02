fef2f234cd7a8e84cc1e61b70c93cfb1b34f6035
thevossenberg <117322172+thevossenberg@users.noreply.github.com>
2022-12-01 17:06:44 -0800

    sql file

--- TechTrade.sql
+++ TechTrade.sql
@@ -15,9 +15,9 @@ CREATE TABLE users (
 );
 
 INSERT INTO `users` (`userID`, `email`, `phone`, `address`, `name_first`, `name_last`, `password`) VALUES
-(1, 'john@miller.com', NULL, '100 St 1598, Z5G35G Downtown, USA', 'John', 'Miller', 'spdi##d54153'),
+(1, 'john@miller.com', '23513585', '100 St 1598, Z5G35G Downtown, USA', 'John', 'Miller', 'sp4153'),
 (2, 'maria@miller.com', '15856135', '100 St 1598, Z5G35G Downtown, USA', 'Maria', 'Miller', '555'),
-(3, 'mike@gmail.com', '+141589135', '155 St 1598 #5, Z5G35G Surrey, Canada', 'Mike', 'Mayer-Hava', 'test');
+(3, 'mike@gmail.com', '+141589135', '155 St 1598 #5, Z5G35G Surrey, Canada', 'Mike', 'Mayer-Hava', '156463');
 
 CREATE TABLE categories (
     categoryID  INT         NOT NULL    AUTO_INCREMENT,
@@ -51,9 +51,8 @@ CREATE TABLE products (
 );
 
 INSERT INTO `products` (`productID`, `name`, `description`, `price`, `quantity`, `quality_new`, `ship_days`, `categoryID`, `sellerID`) VALUES
-(1, 'Apple iPad 10.9 64GB with Wi-Fi 6 (10th Generation) - Silve', 'The Apple iPad 10th Generation is colourfully reimagined to be more capable, more intuitive, and even more fun.', '600.00', 20, 1, 3, 6, 1),
-(2, 'Apple iPhone 14 Pro 128GB - Space Black', 'Experience iPhone in a whole new way with the iPhone 14 Pro.', '450.00', 2, 0, 5, 1, 2),
-(3, 'Razer Viper 8KHz 20000 DPI Optical Gaming Mouse - Black', 'With high performance hardware in a ...', '90.00', 0, 0, 8, 4, 1);
+(1, 'Apple iPad 10.9 64GB with Wi-Fi 6 (10th Generation) - Silver', 'The Apple iPad 10th Generation is colourfully reimagined to be more capable, more intuitive, and even more fun.', '600.00', 20, 1, 3, 6, 1),
+(2, 'Apple iPhone 14 Pro 128GB - Space Black', 'Experience iPhone in a whole new way with the iPhone 14 Pro.', '450.00', 2, 0, 5, 1, 2);
 
 CREATE TABLE orders (
     orderID     INT             NOT NULL    AUTO_INCREMENT,
@@ -65,10 +64,6 @@ CREATE TABLE orders (
     FOREIGN KEY (userID) REFERENCES users (userID)
 );
 
-INSERT INTO `orders` (`orderID`, `userID`, `order_date`, `grand_total`) VALUES
-(1, 3, '2022-10-27 12:17:10', '1340.00'),
-(2, 3, '2022-10-27 12:18:10', '550.00');
-
 CREATE TABLE order_items (
     orderID     INT             NOT NULL,
     productID   INT             NOT NULL,
@@ -79,8 +74,3 @@ CREATE TABLE order_items (
     FOREIGN KEY (orderID) REFERENCES orders(orderID),
     FOREIGN KEY (productID) REFERENCES products(productID)
 );
-
-INSERT INTO `order_items` (`orderID`, `productID`, `amount`, `total`) VALUES
-(1, 1, 2, '1200.00'),
-(1, 3, 1, '90.00'),
-(2, 2, 1, '450.00');
